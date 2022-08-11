ecb0b543c7d800acde5725138054d99c77f51325
lostofan <kolog007@gmail.com>
2022-08-11 15:59:09 +0300

    refactoring

--- src/redux/CounterSlice.ts
+++ src/redux/CounterSlice.ts
@@ -7,6 +7,37 @@ import { CounterStateType, Dots } from "../types/stateTypes";
 import { RootState } from "./store";
 import { v4 as uuid } from 'uuid';
 
+class Powerup {
+    multiplier: number;
+    value: number;
+    price: number;
+    constructor(multiplier:number, value:number, price:number) {
+        this.multiplier = multiplier;
+        this.value = value;
+        this.price = price;
+    }
+}
+const   billy = new Powerup(0.1, 0, 50),
+        ghostface = new Powerup(0.2, 0, 100),
+        pennywise = new Powerup(0.3, 0, 150),
+        pinhead = new Powerup(0.4, 0, 200),
+        chucky = new Powerup(0.5, 0, 250),
+        jason = new Powerup(0.75, 0, 300),
+        freddy = new Powerup(1, 0, 400),
+        myers = new Powerup(1.25, 0, 500),
+        leatherface = new Powerup(1.5, 0, 600),
+        baloon = new Powerup(1.1, 0, 15),
+        mirror = new Powerup(1.2, 0, 40),
+        scythe = new Powerup(1.3, 0, 100),
+        axe = new Powerup(1.5, 0, 150),
+        chainsaw = new Powerup(1.7, 0, 200),
+        knife = new Powerup(2, 0, 250),
+        machete = new Powerup(2.25, 0, 300),
+        glove = new Powerup(2.5, 0, 350),
+        hatchet = new Powerup(3, 0, 450);
+
+
+
 const initialState:CounterStateType = {
     value: 0,
     killerInterval: null,
@@ -17,99 +48,26 @@ const initialState:CounterStateType = {
     loaded: false,
     items: {
         killers: {
-            billy: {
-                multiplier: 0.1,
-                value: 0,
-                price: 50,
-            },
-            ghostface: {
-                multiplier: 0.2,
-                value: 0,
-                price: 100,
-            },
-            pennywise: {
-                multiplier: 0.3,
-                value: 0,
-                price: 150,
-            },
-            pinhead: {
-                multiplier: 0.4,
-                value: 0,
-                price: 200,
-            },
-            chucky: {
-                multiplier: 0.5,
-                value: 0,
-                price: 250,
-            },
-            jason: {
-                multiplier: 0.75,
-                value: 0,
-                price: 300,
-            },
-            freddy: {
-                multiplier: 1,
-                value: 0,
-                price: 400,
-            },
-            myers: {
-                multiplier: 1.25,
-                value: 0,
-                price: 500,
-            },
-            leatherface: {
-                multiplier: 1.5,
-                value: 0,
-                price: 600,
-            }
+            billy,
+            ghostface,
+            pennywise,
+            pinhead,
+            chucky,
+            jason,
+            freddy,
+            myers,
+            leatherface,
     }, weapons: {
-        baloon: {
-            multiplier: 1.1,
-            value: 0,
-            price: 15,
-        },
-        mirror: {
-            multiplier: 1.2,
-            value: 0,
-            price: 40,
-        },
-        scythe: {
-            multiplier: 1.3,
-            value: 0,
-            price: 100,
-        },
-        axe: {
-            multiplier: 1.5,
-            value: 0,
-            price: 150,
-        },
-        chainsaw: {
-            multiplier: 1.7,
-            value: 0,
-            price: 200,
-        },
-        knife: {
-            multiplier: 2,
-            value: 0,
-            price: 250,
-        },
-        machete: {
-            multiplier: 2.25,
-            value: 0,
-            price: 300,
-        },
-        glove: {
-            multiplier: 2.5,
-            value: 0,
-            price: 350,
-        },
-        hatchet: {
-            multiplier: 3,
-            value: 0,
-            price: 450,
-        }
+        baloon,
+        mirror,
+        scythe,
+        axe,
+        chainsaw,
+        knife,
+        machete,
+        glove,
+        hatchet
     }
-
     }
     
 }
@@ -153,7 +111,7 @@ const counterSlice = createSlice({
             } else {
                 state.value += action.payload.value * state.clickMultiplier;
             }
-            
+            console.log(state.items.killers.billys);
             localStorage.setItem('counter', state.value.toFixed().toString());
         },
         addKiller: (state, action:PayloadAction<ActionKillerType>) => {
